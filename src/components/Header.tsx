b461c0c8d70a09636b267a0f610ad7e4a003052a
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-18 17:13:29 +0100

    headerDarkMode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,41 @@
-import hero1 from '../assets/hero1.png'
-import hero2 from '../assets/hero2.png'
-import hero3 from '../assets/hero3.png'
+import hero1 from '../assets/logo.png'
+
 import { Link } from 'react-router-dom'
 const Header = () => {
 	return (
 		<section>
-			<section className="w-4/5 m-auto flex justify-between bg-white">
-				<img className='object-contain flex max-lg:w-1/4' src={hero1} alt="hero" />
-				<img className='object-contain flex max-lg:w-1/3' src={hero2} alt="hero" />
-				<img className='object-contain flex max-lg:w-1/3' src={hero3} alt="hero" />
+			<section className="w-4/5 m-auto flex justify-between bg-white items-center">
+				<div className='flex flex-col justify-center items-center'>
+					<img
+						className="object-contain flex max-lg:w-1/4 w-16"
+						src={hero1}
+						alt="hero"
+					/>
+					<div className='text-xl'>A-M-Info.fr</div>
+					<div className='text-xs italic'>L'info devient un plaisir</div>
+				</div>
+
+				<div>
+					<div>
+						<span className="text-vert-loic">A</span>ssistance,
+					</div>
+					<div className="ml-10">
+						<span className="text-vert-loic">M</span>aintenance,
+					</div>
+					<div className="ml-20">
+						<span className="text-vert-loic">I</span>nformatique.
+					</div>
+				</div>
+
+				<div>
+					<div>7j/7</div>
+					<div>De 9h00 à 20h00</div>
+					<div>06 84 97 66 67</div>
+				</div>
 			</section>
+
 			<section>
-				<Link to={"/"} ></Link>
+				<Link to={'/'}></Link>
 			</section>
 		</section>
 	)
